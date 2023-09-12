d0b5397c67b3ac2f1bfc93f047db2db58c6f6ae3
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-09-12 15:24:41 +0300

    add(lab0): bash file for first task

---
+++ lab0/1_structure_and_content.sh
@@ -0,0 +1,27 @@
+# First level files and catalogues
+touch glalie4
+mkdir gothorita1
+mkdir gulpin3
+touch kabutops7
+touch nidoqueen3
+mkdir serperior2
+
+# Second level files and catalogues
+cd gothorita1
+touch whiscash
+mkdir swadloon
+touch braviary
+mkdir hippowdon
+
+cd ../gulpin3
+touch piloswine
+mkdir medicham
+touch lilligant
+touch prinplup
+
+cd ../serperior2
+mkdir magcargo
+mkdir tentacool
+touch wartortle
+mkdir pignite
+touch gengar
